e4b5d5f1e1d207df1ceeabea650f561f317e6543
Jonathansteven21 <jonathansteven.gc@gmail.com>
2024-06-24 17:39:31 -0500

    api-16: Add DTO and Converter classes for Client and Order entities

---
+++ src/main/java/online/lcelectronics/api/converters/OrderConverter.java
@@ -0,0 +1,33 @@
+package online.lcelectronics.api.converters;
+
+import lombok.RequiredArgsConstructor;
+import online.lcelectronics.api.dto.OrderDTO;
+import online.lcelectronics.api.entities.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class OrderConverter {
+
+    private ClientConverter clientConverter;
+
+    /**
+     * Converts an Order entity to its corresponding DTO.
+     * @param order The Order entity to convert.
+     * @return The converted OrderDTO.
+     */
+    public OrderDTO toDto(Order order) {
+        OrderDTO dto = new OrderDTO();
+        dto.setId(order.getId());
+        dto.setClient(clientConverter.toDto(order.getClient()));
+        dto.setIssue(order.getIssue());
+        dto.setProductReceivedNotes(order.getProductReceivedNotes());
+        dto.setHistoricAppliance(order.getHistoricAppliance());
+        dto.setImages(order.getImages());
+        dto.setCreatedDate(order.getCreatedDate());
+        dto.setStatus(order.getStatus());
+        return dto;
+    }
+}
